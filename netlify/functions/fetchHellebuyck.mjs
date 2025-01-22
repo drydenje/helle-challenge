6e450540883f465b3a8ee72d4bc39947d66da7aa
Jeremy <jeremy.dryden@gmail.com>
2025-01-22 15:26:32 -0500

    added a fetch

--- netlify/functions/fetchHellebuyck.mjs
+++ netlify/functions/fetchHellebuyck.mjs
@@ -4,11 +4,11 @@ import stats from "../../src/stats/hellebuyck";
 // use setJSON to set the blob
 
 export default async (req, context) => {
-  const store = getStore("stats");
-  await store.setJSON("hellebuyck", stats);
+  const url = `https://api-web.nhle.com/v1/player/8476945/game-log/20242025/2`;
+  const resp = await fetch(url);
 
-  // const url = `https://api-web.nhle.com/v1/player/8476945/game-log/20242025/2`;
-  // const resp = await fetch(url);
+  const store = getStore("stats");
+  await store.setJSON("hellebuyck", resp);
 
   return new Response(JSON.stringify(stats));
   // return new Response(JSON.stringify(stats));
