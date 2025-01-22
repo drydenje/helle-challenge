c3ac36d7ea1f10e56884f957b31620ce9a449066
Jeremy <jeremy.dryden@gmail.com>
2025-01-22 13:42:42 -0500

    setting helle stats in store

---
+++ netlify/functions/fetchHellebuyck.mjs
@@ -0,0 +1,15 @@
+import { getStore } from "@netlify/blobs";
+import stats from "../../src/stats/hellebuyck";
+
+// use setJSON to set the blob
+
+export default async (req, context) => {
+  const store = getStore("stats");
+  await store.setJSON("hellebuyck", stats);
+
+  // const url = `https://api-web.nhle.com/v1/player/8476945/game-log/20242025/2`;
+  // const resp = await fetch(url);
+
+  return new Response(stats);
+  // return new Response(JSON.stringify(stats));
+};
