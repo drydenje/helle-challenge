138f810781006d75356f46604d0dc27e3d854290
Jeremy <jeremy.dryden@gmail.com>
2025-01-23 17:33:32 -0500

    changed time to run fetch function, added error codes when things don't work, started work on the chart

--- netlify/functions/hello.mjs
+++ netlify/functions/hello.mjs
@@ -1,12 +1,35 @@
 import { getStore } from "@netlify/blobs";
-// import type { Context } from "@netlify/functions"
-// import stats from "../../src/stats/price";
-
-// use setJSON to set the blob
 
 export default async (req, context) => {
   const store = getStore("stats");
   const helle = await store.get("hellebuyck");
-  return new Response(helle);
-  // return new Response(JSON.stringify(stats));
+  // needed to use a temp var, might be able to parse without?
+  const temp = JSON.parse(helle);
+  const res = Object.assign(
+    // create a new blank object
+    {},
+    // add the player name and number first
+    // (this is done for a nicer viewing order, not js related)
+    {
+      playerName: "Connor Hellebuyck",
+      playerNumber: 8476945,
+    },
+    // then add the blob data pulled from netlify
+    temp,
+    // then remove the fields you don't want
+    {
+      gameTypeId: undefined,
+      playerStatsSeasons: undefined,
+    }
+  );
+
+  // check if data is stored in a blob
+  if (res === null)
+    return new Response("No data received", {
+      status: 500,
+      statusText: "Failed to retrive blob",
+    });
+
+  // Everything's ok, return the data
+  return new Response(JSON.stringify(res));
 };
--- netlify/functions/price.mjs
+++ netlify/functions/price.mjs
@@ -1,5 +1,24 @@
 import stats from "../../src/stats/price";
 
 export default async (req, context) => {
-  return new Response(JSON.stringify(stats));
+  const temp = stats;
+  const res = Object.assign(
+    // create a new blank object
+    {},
+    // add the player name and number first
+    // (this is done for a nicer viewing order, not js related)
+    {
+      playerName: "Carey Price",
+      playerNumber: 8471679,
+    },
+    // then add the blob data pulled from netlify
+    temp,
+    // then remove the fields you don't want
+    {
+      gameTypeId: undefined,
+      playerStatsSeasons: undefined,
+    }
+  );
+  return new Response(JSON.stringify(res));
+  // return new Response(res);
 };
