ae330e519a52b6d2025d0059761b070002a23c02
Jeremy <jeremy.dryden@gmail.com>
2025-01-22 23:01:27 -0500

    added a time to run fetchHellebuyck (3:00 am)

---
+++ netlify/functions/hello.mjs
@@ -0,0 +1,12 @@
+import { getStore } from "@netlify/blobs";
+// import type { Context } from "@netlify/functions"
+// import stats from "../../src/stats/price";
+
+// use setJSON to set the blob
+
+export default async (req, context) => {
+  const store = getStore("stats");
+  const helle = await store.get("hellebuyck");
+  return new Response(helle);
+  // return new Response(JSON.stringify(stats));
+};
