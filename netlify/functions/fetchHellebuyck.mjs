2789a2c071c4f925c129fb653f566d28a6f3263f
Jeremy <jeremy.dryden@gmail.com>
2025-01-26 23:10:33 -0500

    blob fetches are working, multiple lines can be drawn

--- netlify/functions/fetchHellebuyck.mjs
+++ netlify/functions/fetchHellebuyck.mjs
@@ -1,7 +1,4 @@
 import { getStore } from "@netlify/blobs";
-import stats from "../../src/stats/hellebuyck";
-
-// use setJSON to set the blob
 
 export default async (req, context) => {
   const url = `https://api-web.nhle.com/v1/player/8476945/game-log/20242025/2`;
