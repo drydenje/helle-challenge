75acc717414f5ffc6d0d760eb846070ef3cf75c4
Jeremy <jeremy.dryden@gmail.com>
2025-01-27 13:46:11 -0500

    cleaned up fetch call

--- netlify/functions/fetchHellebuyck.mjs
+++ netlify/functions/fetchHellebuyck.mjs
@@ -3,9 +3,10 @@ import { getStore } from "@netlify/blobs";
 export default async (req, context) => {
   const url = `https://api-web.nhle.com/v1/player/8476945/game-log/20242025/2`;
   const resp = await fetch(url);
+  const json = await resp.json();
 
   const store = getStore("stats");
-  await store.setJSON("hellebuyck", await resp.json());
+  await store.setJSON("hellebuyck", await json);
 
-  return new Response(JSON.stringify(await resp.json()));
+  return new Response(json);
 };
